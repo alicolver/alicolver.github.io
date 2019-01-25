c62d4c298984dd63e73cbf8f0adc13f4875ec7d0
Alisdair Colver <ajc4617@ic.ac.uk>
2019-01-25 19:09:06 +0000

    removed scrolling text animation

--- js/pause.js
+++ js/pause.js
@@ -1,10 +1,8 @@
 // Get the video
 var video = document.getElementById("bkgVideo");
-        
-// Get the button
+
 var btn = document.getElementById("pauseBtn");
     
-// Pause and play the video, and change the button text
 function pauseVideo() {
     if (video.paused) {
         video.play();
