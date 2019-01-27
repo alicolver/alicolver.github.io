ea4b81a99e2553790d935b85b97f1e7b70f041c2
Alisdair Colver <ajc4617@ic.ac.uk>
2019-01-27 13:24:31 +0000

    Added in gif for mobile and support for narrow aspect ratio

--- js/selectcss.js
+++ js/selectcss.js
@@ -2,6 +2,8 @@ function adjustStyle(width) {
     width = parseInt(width);
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
       $("#size-stylesheet").attr("href", "css/mobile.css");
+    } else if (width < 900) {
+      $("#size-stylesheet").attr("href", "css/narrow.css");
     } else {
        $("#size-stylesheet").attr("href", "css/index.css"); 
     }
